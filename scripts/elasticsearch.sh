bd71de0e080cbe6e88396247b0269c279f189570
mmotel <mateuszmotel@gmail.com>
2013-12-16 20:42:46 +0100

    zmieniłem typ instalcji ES'a na źródło jar

--- scripts/elasticsearch.sh
+++ scripts/elasticsearch.sh
@@ -1,12 +1,12 @@
 #Elasticsearch 0.90.7
 
-#Open JDK 7
-sudo apt-get update
-sudo apt-get install openjdk-7-jre-headless -y
+cd ~
+mkdir elasticsearch
+cd elasticsearch
 
 #Elasticsearch
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.deb
-sudo dpkg -i elasticsearch-0.90.7.deb
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.tar.gz
+tar -xvf elasticsearch-0.90.7.tar.gz
 
 #ElasticSearch-Head plugin
-sudo /usr/share/elasticsearch/bin/plugin -install mobz/elasticsearch-head  # path for version 0.90
+./elasticsearch/elasticsearch-0.90.7/bin/plugin -install mobz/elasticsearch-head  # path for version 0.90
